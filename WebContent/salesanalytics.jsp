046b80f096eb3237601bf402a08099345ca1a43d
Samuel Adams <samueladamsonline@gmail.com>
2017-05-30 11:11:53 -0700

    Hiding initial form on sales analytics page after Run Query clicked. Also, moved previous form parameters into session variables.

--- WebContent/salesanalytics.jsp
+++ WebContent/salesanalytics.jsp
@@ -1,217 +1,139 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<%@page import="java.sql.Connection, ucsd.shoppingApp.ConnectionManager, ucsd.shoppingApp.*"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ page import="java.sql.Connection, ucsd.shoppingApp.ConnectionManager, ucsd.shoppingApp.*"%>
 <%@ page import="ucsd.shoppingApp.models.* , java.util.*" %>    
 <%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
     
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Sales Analytics</title>
-
-<script src="javascript/jquery-3.2.1.min.js"></script>
-<script src="javascript/basics.js"></script>
-
-	<%
-	
-
-	boolean b;
-	boolean firsttime = (boolean) session.getAttribute("firsttime");
+<%
+	//boolean firsttime = (boolean) session.getAttribute("firsttime");
 	//session.setAttribute("show20",false);
 	
-	System.out.println("counter: "+Integer.toString((Integer)session.getAttribute("counter")));
-	
+	//System.out.println("counter: "+ Integer.toString((Integer) session.getAttribute("counter")));
 	
 	List<String> row_vals = new ArrayList<>();
 	List<String> col_vals = new ArrayList<>();
 	HashMap<String, Map <String,Integer>> cell_vals = new HashMap<>();	
 	Map<String, Integer> totalSales = new HashMap<>();
 	
-	if(request.getAttribute("row_values") != null &&
+	if (request.getAttribute("row_values") != null &&
 		request.getAttribute("col_values") != null &&
 		request.getAttribute("cell_values") != null &&
-		request.getAttribute("totalSales") != null){
+		request.getAttribute("totalSales") != null) {
 		
 		System.out.println("setting attributes");
 		row_vals = (List<String>) request.getAttribute("row_values"); 
 		col_vals = (List<String>) request.getAttribute("col_values");
 		cell_vals = (HashMap<String, Map <String,Integer>>) request.getAttribute("cell_values");
 		totalSales = (Map<String, Integer>) request.getAttribute("totalSales");
-
-		
-
-		//pageContext.setAttribute("sales_per_customer",cell_vals);
-		
-		
-		//start setting up the table. get the column values
 	}
-	
-		%>
-
+%>
+    
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Sales Analytics</title>
 </head>
 <body>
-
-<a href="./home.jsp">Go back home</a>
-
-<%
-
-Connection con = ConnectionManager.getConnection();	
-
-CategoryDAO categoryDao = new CategoryDAO(con);
-List<CategoryModel> category_list = categoryDao.getCategories();
-con.close();
-
-%>
-
-
-<form name="analyzeSales" method="POST" action="SalesAnalyticsController">
-
-	<table>
-	
-	<tr>	
-	<td>Row option:</td>	
-	<td>
-	<select name="row">
-		<option value="customer">Customer</option>
-		<option value="state">State</option>
-	</select>
-	</td>
-	</tr>
-	
-	<tr>
-	<td>Order option:</td>	
-	<td>
-	<select name="order">
-		<option value="alphabetical">Alphabetical</option>
-		<option value="top-k">Top-K</option>
-	</select>	
-	</td>
-	</tr>
-		
-	<tr>
-	<td>Sales filtering option:</td>
-	<td>
-	<select name= "filter">	
-	<option value="all_products">all products</option>
-	<%
-		for (CategoryModel cat : category_list) {
-	%>
-		<option value="<%=cat.getCategoryName()%>"> "<%=cat.getCategoryName() %>"</option>
-	<%
-		}
-	%>
-	</select>	
-	</td>
-	</tr>
-
-	</table>
-	<input type="submit" value="analyze" name="action" />
-
-	
-	<input type="submit" value="next 20" name="action" />
-	<input type="submit" value="reset" name="action" />
-	<input type="submit" value="next 10 products" name="action" />
-</form>
-
-
-
-<form action="SalesAnalyticsController" method="post">
-
-
-	
-<br>
-
-
-<table border="1">
-
- <%
- /* DON'T DELETE
-	<tr>
-		<td>XXX</td>
-	<c:forEach items="${products}" var="current">
-		<td> <c:out value="${current}"/> </td>
-	</c:forEach>	
-	<tr>
-	<c:forEach items="${customers}" var="current">
-		<tr> 
-		<td> <c:out value="${current}"/> </td>		
-		</tr>
-	</c:forEach>
-	*/
-	%>
-	
-	<tr>
-	<td> </td>
-	
-<%
-	//setting up the column headers
-	
-	for (int i = 0; i < col_vals.size(); i++){
-		%>
-		<td><b><%=col_vals.get(i) %></td>
-		<%	
-		
-	}
-
-%>	
-	</tr>
-	<%
-	//looping through the rows
-	Map <String,Integer> userSales;
-
-	int sale = 0;
-	
-	for (int i = 0; i < row_vals.size(); i++){
-
-		%>
+<a href="./home.jsp">Back to Home</a>
+<c:choose>
+	<c:when test="${sessionScope.firsttime eq null}">
+		<form action="SalesAnalyticsController" method="post" >
+			<table>
+				<tr>
+					<td>Row option:</td>
+					<td>
+						<select name="row">
+							<option value="customer">Customer</option>
+							<option value="state">State</option>
+						</select>
+					</td>
+				</tr>
+				<tr>
+					<td>Order option:</td>
+					<td>
+						<select name="order">
+							<option value="alphabetical">Alphabetical</option>
+							<option value="top-k">Top-K</option>
+						</select>
+					</td>
+				</tr>
+				<tr>
+					<td>Sales filtering option:</td>
+					<td>
+						<select name="filter">
+							<option value="all_products">All Products</option>
+							<%
+								Connection con = ConnectionManager.getConnection();
+								CategoryDAO categoryDao = new CategoryDAO(con);
+								List<CategoryModel> category_list = categoryDao.getCategories();
+								con.close();
+							
+								for (CategoryModel cat : category_list) {
+							%>
+							<option value="<%=cat.getCategoryName()%>">"<%=cat.getCategoryName()%>"</option>
+							<%
+								}
+							%>
+						</select>
+					</td>
+				</tr>
+			</table>
+			<input type="submit" value="Run Query" name="action" />
+		</form>
+	</c:when>
+	<c:otherwise>
+		<form action="SalesAnalyticsController" method="post">
+			<input type="submit" value="Next 20 Rows" name="action" />
+			<input type="submit" value="Next 10 Columns" name="action" />
+			<input type="submit" value="Reset" name="action" />
+		</form>
+		<table border="1">
 		<tr>
-		<% 
-		
-		String user;
-		user = row_vals.get(i);
-		
-		
-		userSales = cell_vals.get(user);
-			//display the user name/state (row value) and total purchases made
-
-		
-			
-		%>
-		<td><b> <%=user+" ("+Integer.toString(totalSales.get(user))+")" %></b></td>
-		<% 
-
-		
-		//loop through col vals to output cell values in correct order
-		for (int j = 0; j < col_vals.size(); j++){
-			String temp;
-			temp = col_vals.get(j);
-			if (userSales.get(temp) != null){
-			sale = userSales.get(temp);
-			}
+			<td></td>
+			<%
+				//setting up the column headers
+				for (int i = 0; i < col_vals.size(); i++) {
 			%>
-			<td><%=sale %></td>
-			<% 
-		}
+			<td><b><%= col_vals.get(i) %></b></td>
+			<%
+				}
+			%>
+		</tr>
+		<tr>
+			<%
+				//looping through the rows
+				Map <String, Integer> userSales;
+				int sale = 0;
 	
-		%>
+				for (int i = 0; i < row_vals.size(); i++) {
+			
+				String user;
+				user = row_vals.get(i);
+		
+				userSales = cell_vals.get(user);
+				//display the user name/state (row value) and total purchases made
+			%>
+			<td><b><%= user + " (" + Integer.toString(totalSales.get(user)) + ")" %></b></td>
+			<% 
+				//loop through col vals to output cell values in correct order
+				for (int j = 0; j < col_vals.size(); j++) {
+					String temp;
+					temp = col_vals.get(j);
+				
+					if (userSales.get(temp) != null) {
+						sale = userSales.get(temp);
+					}
+			%>
+			<td><%= sale %></td>
+			<% 
+				}
+			%>
 		</tr>
 		<% 
-	}
-	
-	
-	%>
-	
-	
-
-</table>
-
-</form>
-	
-	
-	
-
+			}
+		%>
+		</table>
+	</c:otherwise>
+</c:choose>
 </body>
 </html>
-
-
--- src/ucsd/shoppingApp/controllers/SalesAnalyticsController.java
+++ src/ucsd/shoppingApp/controllers/SalesAnalyticsController.java
@@ -126,17 +126,24 @@ public class SalesAnalyticsController extends HttpServlet {
 	 */
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		//doGet(request, response);
+		
+		String action = request.getParameter("action");
 		
 		HttpSession session = request.getSession();
 		
-		response.setContentType("text/html");
-		
-		String row_option = request.getParameter("row");
-		String order_option = request.getParameter("order");
-		String sales_filter_option = request.getParameter("filter");
-		String action = request.getParameter("action");
+		if (action.equals("Run Query")) {
+			
+			// Set filter session variables.
+			session.setAttribute("row", request.getParameter("row"));
+			session.setAttribute("order", request.getParameter("order"));
+			session.setAttribute("filter", request.getParameter("filter"));
+		    session.setAttribute("firsttime", false);
+		}
+
+		// Get filter session variables.
+		String rowOption = (String) session.getAttribute("row");
+		String orderOption = (String) session.getAttribute("order");
+		String salesFilterOption = (String) session.getAttribute("filter");
 
 		ResultSet rs = null;
 		ResultSet rc = null;
@@ -145,7 +152,6 @@ public class SalesAnalyticsController extends HttpServlet {
 	    PreparedStatement pstmt2 = null;
 	    
 	    Connection conn = ConnectionManager.getConnection();
-	    session.setAttribute("firsttime", false);
 
 		try{
 			Statement statement = conn.createStatement();
@@ -207,28 +213,34 @@ public class SalesAnalyticsController extends HttpServlet {
 		//maps customer_name to hashmap
 		HashMap< String, Map <String,Integer>> totalsales_per_rowval = new HashMap<>();
 
-		if (action.equalsIgnoreCase("reset")){
-			session.setAttribute("firsttime", true);
+		// Reset button clicked so resetting session variables.
+		if (action.equalsIgnoreCase("Reset")) {
+			session.removeAttribute("firsttime");
+			session.removeAttribute("row");
+			session.removeAttribute("order");
+			session.removeAttribute("filter");
 		}
-	    		
-		else if (action.equalsIgnoreCase("analyze") || action.equalsIgnoreCase("next 20") ) {
-			try{
-				if (row_option.equals("customer") || row_option.equals("state")) {
-					if (order_option.equals("alphabetical")|| (order_option.equalsIgnoreCase("top-k"))){
+	    
+		else if (action.equalsIgnoreCase("Run Query") || action.equalsIgnoreCase("Next 20 Rows") ) {
+			try {
+				
+				if (rowOption.equalsIgnoreCase("customer") || rowOption.equalsIgnoreCase("state")) {
+					if (orderOption.equalsIgnoreCase("alphabetical") || orderOption.equalsIgnoreCase("top-k")) {
 					  	
-						if(action.equalsIgnoreCase("analyze") && row_option.equalsIgnoreCase("customer")){
+						if (action.equalsIgnoreCase("Run Query") && rowOption.equalsIgnoreCase("customer")) {
 							//reset the counter
 							reset_offset(request, response);
 							pstmt.setInt(1, 0);
 							rs = pstmt.executeQuery();
 						}
-						else if (action.equalsIgnoreCase("next 20") && row_option.equalsIgnoreCase("customer")){
+						else if (action.equalsIgnoreCase("Next 20 Rows") && rowOption.equalsIgnoreCase("customer")) {
+							System.out.println("inside");
 							//increase the counter
-							increase_offset(request,response);
+							increase_offset(request, response);
 							pstmt.setInt(1, (int) session.getAttribute("counter"));
 							rs = pstmt.executeQuery();
 						}
-						else if(action.equalsIgnoreCase("analyze") && row_option.equalsIgnoreCase("state")){
+						else if (action.equalsIgnoreCase("Run Query") && rowOption.equalsIgnoreCase("state")) {
 							//reset the counter
 							reset_offset(request, response);
 							pstmt2.setInt(1, 0);
@@ -236,11 +248,10 @@ public class SalesAnalyticsController extends HttpServlet {
 						}
 						else{
 							//increase the counter
-							increase_offset(request,response);
+							increase_offset(request, response);
 							pstmt2.setInt(1, (int) session.getAttribute("counter"));
 							rs = pstmt2.executeQuery();	
 						}
-
 
 						//populate row_vals with values for the rows
 						String tmp;
@@ -257,10 +268,10 @@ public class SalesAnalyticsController extends HttpServlet {
 						//3. get the values into hash map and insert into totalsales_per_rowval
 						
 						String row_val;
-						for (int i =0; i < row_vals.size(); i++) {
+						for (int i = 0; i < row_vals.size(); i++) {
 							row_val = row_vals.get(i);
 							
-							if (row_option.equals("customer")){
+							if (rowOption.equalsIgnoreCase("customer")) {
 								pstmt = conn.prepareStatement(getGrandTotalTable);
 							}
 							else{
@@ -310,17 +321,16 @@ public class SalesAnalyticsController extends HttpServlet {
 						//now get the product names. if all_producuts was chosen, get
 						//all products. else, get only the category chosen...
 						
-						if (sales_filter_option.equals("all_products")){
+						if (salesFilterOption.equalsIgnoreCase("all_products")) {
 					
 							rt = statement1.executeQuery("select * "
 														+ "from product "
 														+ "order by product_name "
-														+ "limit 10 ");					
-
+														+ "limit 10 ");
 							
 							//System.out.println("obtained result set for products");
 							String prod;
-							while (rt.next()){
+							while (rt.next()) {
 								prod = rt.getString(3);
 				
 								col_vals.add(prod);							
@@ -332,11 +342,11 @@ public class SalesAnalyticsController extends HttpServlet {
 							
 							pstmt = conn.prepareStatement("select p.product_name from product p, category c "+
 														 "where p.category_id = c.id and "+
-														 "c.category_name= ? "
-														 + "order by product_name "+
+														 "c.category_name= ? "+
+														 "order by product_name "+
 														 "limit 10 ");
 
-							pstmt.setString(1, sales_filter_option);
+							pstmt.setString(1, salesFilterOption);
 							
 							rt = pstmt.executeQuery();
 							
@@ -354,7 +364,7 @@ public class SalesAnalyticsController extends HttpServlet {
 						//if the TOP-K ordering was chosen, need to order the rows according to
 						//how much money was spent
 
-						if (order_option.equalsIgnoreCase("top-k")){
+						if (orderOption.equalsIgnoreCase("top-k")) {
 							
 							//need to sort the total sales
 							MyPair[] list = new MyPair[row_vals.size()];
@@ -367,24 +377,23 @@ public class SalesAnalyticsController extends HttpServlet {
 								MyPair temp = new MyPair(entry.getKey(),entry.getValue());
 								
 								i++;
-
 						    }
 							System.out.println("just filled in the list");
 							bubbleSort(list);
 							System.out.println("just sorted");
+							
 							for (int a =0; a<list.length;a++){
 								System.out.println(Integer.toString(list[a].value));
 							}
-							
 						}
-
 					}
-				
 				}		
-			}catch (Exception e) {
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
 		}
-		//System.out.println("setting attribute");
+		
+		// Set table session variables.
 		request.setAttribute("row_values",row_vals);
 		request.setAttribute("col_values",col_vals);
 		request.setAttribute("cell_values", totalsales_per_rowval);
@@ -394,26 +403,26 @@ public class SalesAnalyticsController extends HttpServlet {
 
 		//statement.close();
 		}
-		catch(SQLException e){
-			
+		catch(SQLException e) {
+			e.printStackTrace();
 		}
 		finally {
-		    // Release resources in a finally block in reverse-order of
-		    // their creation
-			    if (rs != null && rc != null && rt != null && pstmt != null && conn != null) {
-			        try {
-			            rs.close();
-			            rc.close();
-			            rt.close();
-			            pstmt.close();
-			            conn.close();
-			        } catch (SQLException e) { } // Ignore
-			        rs = null;
-			        rt = null;
-			        rc = null;
-			        pstmt = null;
-			        conn = null;
-			    }		    
-		}		
-	}	
+			if (rs != null && rc != null && rt != null && pstmt != null && conn != null) {
+				try {
+					rs.close();
+					rc.close();
+					rt.close();
+					pstmt.close();
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+				rs = null;
+				rt = null;
+				rc = null;
+				pstmt = null;
+				conn = null;
+			}
+		}
+	}
 }
