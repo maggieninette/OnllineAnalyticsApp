123adb256109595079cf2744ed7d91c4a4efda7e
Samuel Adams <samueladamsonline@gmail.com>
2017-05-30 14:09:08 -0700

    Removed unused import statements.

--- src/ucsd/shoppingApp/controllers/SalesAnalyticsController.java
+++ src/ucsd/shoppingApp/controllers/SalesAnalyticsController.java
@@ -2,23 +2,13 @@ package ucsd.shoppingApp.controllers;
 
 import java.io.IOException;
 
-import java.io.PrintWriter;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.SortedMap;
-import java.util.TreeMap;
 
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -26,10 +16,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.google.gson.Gson;
-
 import ucsd.shoppingApp.ConnectionManager;
-import ucsd.shoppingApp.Pair;
 import ucsd.shoppingApp.PersonDAO;
 import ucsd.shoppingApp.ProductDAO;
 import ucsd.shoppingApp.StateDAO;
